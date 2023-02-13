f22a9ee448f3fd1f26b9eba0c726f2011b08e5e7
Vitaliy Shilkin <vito_jj@mail.ru>
2023-02-13 15:09:13 +0300

    Add DBMS and UI-Tests

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,150 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Locale;
+
+public class DataGenerator {
+    public static Faker faker = new Faker(new Locale("en"));
+
+    public static String getApprovedNumber() {
+        return "4444 4444 4444 4441";
+    }
+
+    public static String getDeclinedNumber() {
+        return "4444 4444 4444 4442";
+    }
+
+    public static String getInvalidCardNumber() {
+        return "1111 4444 4444 8888";
+    }
+
+    public static String getShortCardNumber() {
+        return "4444 4444 4444 444";
+    }
+
+    public static String getEmptyNumber() {
+        return null;
+    }
+
+    public static String getCurrentMonth() {
+        LocalDate localDate = LocalDate.now();
+        return String.format("%02d", localDate.getMonthValue());
+    }
+
+    //отнимает не месяц, а год, как сделать месяц?
+    public static String getLastMonth() {
+        LocalDate localDate = LocalDate.now();
+        LocalDate lastMonth = localDate.minusMonths(1);
+        return String.valueOf(lastMonth);
+
+    }
+
+    public static String getInvalidMonth() {
+        return "00";
+    }
+
+    public static String getNonExistingMonth() {
+        return "15";
+    }
+
+    public static String getEmptyMonth() {
+        return null;
+    }
+
+    public static String getCurrentYear() {
+        return String.format("%ty", Year.now());
+    }
+
+    public static String getLastYear() {
+        LocalDate localDate = LocalDate.now();
+        return String.format("%ty", localDate.minusYears(1));
+    }
+
+    public static String getNextYear() {
+        LocalDate localDate = LocalDate.now();
+        return String.format("%ty", localDate.plusYears(1));
+    }
+
+    public static String getEmptyYear() {
+        return null;
+
+    }
+
+    public static String getValidName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String getValidNameInLatinLetters() {
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String getOnlyName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().firstName();
+    }
+
+    public static String getOnlyNameInLatinLetters() {
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().firstName();
+    }
+
+    public static String getOnlySurname() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().lastName();
+    }
+
+    public static String getOnlySurnameInLatinLetters() {
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().lastName();
+    }
+
+    public static String getNameWithDash() {
+        return "Виталий-Шилкин";
+    }
+
+    public static String getTooLongName() {
+        return faker.lorem().fixedString(200);
+    }
+
+    public static String getNameWithNumbers() {
+        return faker.number().digits(9);
+    }
+
+    public static String getNameWithOneLetter() {
+        return faker.lorem().characters(1);
+    }
+
+    public static String getEmptyName() {
+        return null;
+    }
+
+    public static String getNameWithSpace() {
+        return " ";
+    }
+
+    public static String getValidCvc() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
+        return fakeValuesService.numerify("###");
+    }
+
+    public static String getCvcWithOneDigit() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
+        return fakeValuesService.numerify("#");
+    }
+
+    public static String getCvcWithTwoDigits() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
+        return fakeValuesService.numerify("##");
+    }
+
+    public static String getEmptyCvc() {
+        return null;
+    }
+}
---
+++ src/test/java/ru/netology/page/PaymentPage.java
@@ -0,0 +1,83 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.Card;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class PaymentPage {
+    private SelenideElement heading = $$("h3").findBy(Condition.text("Оплата по карте"));
+    private SelenideElement cardNumberField = $("input[placeholder='0000 0000 0000 0000']");
+    private SelenideElement monthField = $("input[placeholder='08']");
+    private SelenideElement yearField = $("input[placeholder='22']");
+    private SelenideElement holderField = $(byXpath("/html/body/div/div/form/fieldset/div[3]/span/span[1]/span/span/span[2]/input"));
+    private SelenideElement cvcField = $("input[placeholder='999']");
+    private SelenideElement continueButton = $$("button").findBy(Condition.text("Продолжить"));
+
+
+    public PaymentPage() {
+        heading.shouldBe(visible);
+    }
+
+    public void fulfillData(Card card) {
+        cardNumberField.setValue(card.getNumber());
+        monthField.setValue(card.getMonth());
+        yearField.setValue(card.getYear());
+        holderField.setValue(card.getHolderName());
+        cvcField.setValue(card.getCvc());
+        continueButton.click();
+
+    }
+
+
+    public void successNotification() {
+        $(".notification_status_ok").shouldBe(visible, Duration.ofMillis(15000));
+    }
+
+    public void declineNotification() {
+        $("notification_status_error").shouldBe(visible, Duration.ofMillis(15000));
+    }
+
+    public void invalidFormat() {
+        $(".input__sub").shouldHave(exactText("Неверный формат")).shouldBe(visible);
+    }
+
+    public void requiredField() {
+        $(".input__sub").shouldHave(exactText("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    public void invalidDate() {
+        $(".input__sub").shouldHave(exactText("Неверно указан срок действия карты")).shouldBe(visible);
+    }
+
+    public void expiredDate() {
+        $(".input__sub").shouldHave(exactText("Истёк срок действия карты")).shouldBe(visible);
+    }
+
+    public void invalidName() {
+        $(".input__sub").shouldHave(exactText("Введите полное имя и фамилию")).shouldBe(visible);
+    }
+
+    public void longName() {
+        $(".input__sub").shouldHave(exactText("Значение поля не может содержать более 100 символов")).shouldBe(visible);
+    }
+
+    public void invalidDataName() {
+        $(".input__sub").shouldHave(exactText("Значение поля может содержать только буквы и дефис")).shouldBe(visible);
+    }
+
+    public void shortName() {
+        $(".input__sub").shouldHave(exactText("Значение поля должно содержать больше одной буквы")).shouldBe(visible);
+    }
+
+    public void invalidCvc() {
+        $(".input__sub").shouldHave(exactText("Значение поля должно содержать 3 цифры")).shouldBe(visible);
+    }
+}
